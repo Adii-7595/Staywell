b1d87c7eaea8f90090299f3e7d896ea86bfc5e6e
Aditya Gupta <rajaditya2399@gmail.com>
2024-11-01 14:15:21 +0530

    restructuring express routes and creating routes folder

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path")
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const wrapAsync = require('./utils/wrapAsync.js')
 const ExpressError = require('./utils/ExpressError.js')
-const {listingSchema}= require('./schema.js')
+const listings = require('./routes/listing.js')
+const reviews = require('./routes/review.js')
+
 require('dotenv').config();
-
-
 
 const MONGO_URL = process.env.MONGO_URL;
 
@@ -27,7 +25,6 @@ async function main() {
 
 }
 
-
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -36,89 +33,15 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
+
 app.get("/", (req, res) => {
     res.send("hi I'm root!");
 });
 
-const validateListing = (req,res,next) => {
-    let {error} = listingSchema.validate(req.body)
-    console.log(error)
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",") 
-        throw new ExpressError(400,errMsg)
-    }else{
-        next(); 
-    }
-};
-//show all posts
-
-app.get("/listings", wrapAsync(async (req, res, next) => {
-    try {
-        const allListings = await Listing.find({});
-
-        res.render("listings/index.ejs", { allListings });
-    } catch (error) {
-        console.error("Error fetching listings:", error);
-        res.status(500).send("Internal Server Error");
-    }
-}))
-
-app.get("/listings/new", (req, res) => {
-    res.render("listings/form.ejs")
-});
+app.use('/listings', listings)
+app.use('/listings/:id/reviews', reviews)
 
 
-//show route for individual post
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id)
-
-    res.render("listings/show.ejs", { listing })
-})
-)
-
-
-
-
-//create route
-app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
-    const newListing = new Listing(req.body.listing)
-    
-    console.log(newListing);
-    await newListing.save()
-    res.redirect("/listings");
-})
-
-);
-
-app.get('/listings/:id/edit', wrapAsync(async (req, res) => {
-
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,'send valid data!')
-    // }
-    let { id } = req.params;
-
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing })
-}) 
-)
-
-app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-})
-)
-
-
-//delete route
-app.delete('/listings/:id', wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedList = await Listing.findByIdAndDelete(id);
-    console.log(deletedList);
-    res.redirect('/listings');
-})
-)
 
 app.all('*', (req, res, next) => {
     next(new ExpressError(404, 'Page Not Found!'))
@@ -127,8 +50,8 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
 
-    let { statusCode =500, message='Something went wroong!' } = err;
-    res.status(statusCode).render("error.ejs",{err})
+    let { statusCode = 500, message = 'Something went wroong!' } = err;
+    res.status(statusCode).render("error.ejs", { err })
 })
 
 app.listen(port, () => {
