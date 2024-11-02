76b332ec36bfd5690fe47736ff39e7a874a6138c
Aditya Gupta <rajaditya2399@gmail.com>
2024-11-02 13:56:55 +0530

    registration and authentication

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const path = require("path")
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js')
-const listings = require('./routes/listing.js')
-const reviews = require('./routes/review.js')
+
+
+const listingRouter = require('./routes/listing.js')
+const reviewRouter = require('./routes/review.js')
+const userRouter = require('./routes/user.js')
+
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/user.js")
+
 
 
 require('dotenv').config();
@@ -50,24 +58,43 @@ app.get("/", (req, res) => {
     res.send("hi I'm root!");
 });
 
-
+//session manangement and cookies middlewares
 
 app.use(session(sessionOptions))
 app.use(flash())
 
+
+//passport session for login middlewares
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+//middleware for flashing message (alert)
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success")
     res.locals.error = req.flash("error")
-    console.log(res.locals.success)
     next()
 })
 
 
+// app.get('/demouser',async(req,res)=>{
+//     let fakeUser = new User({
+//         email:"student@gmail.com",
+//         username:"adityagupta"
+//     })
+
+//    let registeredUser = await User.register(fakeUser,"helloBrother")
+//    res.send(registeredUser)
+
+// })
 
 
-
-app.use('/listings', listings)
-app.use('/listings/:id/reviews', reviews)
+app.use('/listings', listingRouter)
+app.use('/listings/:id/reviews', reviewRouter)
+app.use('/',userRouter)
 
 
 
@@ -78,7 +105,7 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
 
-    let { statusCode = 500, message = 'Something went wroong!' } = err;
+    let { statusCode = 500, message = 'Something went wrong!' } = err;
     res.status(statusCode).render("error.ejs", { err })
 })
 
